3cbb1d3d191e831c7a839e87186d07c52b8e90cc
lauriezip <lauriezip@gmail.com>
2017-11-06 14:37:57 -0500

    Added new file "EventDetailViewController"

---
+++ Car Pool/EventDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  EventDetailViewController.swift
+//  Car Pool
+//
+//  Created by Laurie Zipperer on 11/6/17.
+//  Copyright © 2017 Laurie Zipperer. All rights reserved.
+//
+
+import UIKit
+
+class EventDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
+
+

4f5746a9dd4de83574c248d4d7d337b8ba2af3f6
Joshua Dodd <joshuadodd@gmail.com>
2017-11-07 16:15:20 -0500

    update datepicker

--- Car Pool/CreateTripViewController.swift
+++ Car Pool/CreateTripViewController.swift
@@ -12,15 +12,31 @@ import CarpoolKit
 
 class CreateTripViewController: UIViewController{
     
-    @IBOutlet weak var onDatePicker: UIDatePicker!
-    
+    @IBOutlet weak var datePickerView: UIDatePicker!
+    var selectDate = Date()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        datePickerView.date = selectDate
         
 //        API.createTrip(eventDescription: <#T##String#>, eventTime: <#T##Date#>, eventLocation: <#T##CLLocation#>, completion: <#T##(Result<Trip>) -> Void#>)
     }
     
+    @IBAction func onDatePickerButton(_ sender: Any) {
+        let secondsSinceOriginDate = Date().timeIntervalSince(datePickerView.minimumDate!)
+        let day = arc4random_uniform(UInt32(secondsSinceOriginDate / 60 / 60 / 24))+1
+        let today = Date(timeIntervalSinceNow: 0)
+        let gregorian  = NSCalendar(calendarIdentifier: NSCalendar.Identifier.gregorian)
+        var offsetComponents = DateComponents()
+        offsetComponents.day = -Int(day - 1)
+        let randomDate = gregorian?.date(byAdding: offsetComponents, to: today, options: .init(rawValue: 0) )
+        datePickerView.date = randomDate!
+        
+        
+        
+        
+        
+    }
     
 }
--- Car Pool/TripDetailViewController.swift
+++ Car Pool/TripDetailViewController.swift
@@ -34,6 +34,8 @@ class TripDetailViewController: UIViewController {
 
     }
  
-    
+    func showTripDetails() {
+        
+    }
    
 }

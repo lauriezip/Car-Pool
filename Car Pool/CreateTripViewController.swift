5a6bc92396af0e9715900f9f875dc3746f24c41a
Lauriezip <lauriezip@gmail.com>
2017-11-09 11:23:20 -0500

    claim added

--- Car Pool/CreateTripViewController.swift
+++ Car Pool/CreateTripViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CarpoolKit
+import FirebaseCommunity
+import PromiseKit
 
 
 
--- Car Pool/TripDetailViewController.swift
+++ Car Pool/TripDetailViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CarpoolKit
+import FirebaseCommunity
+import PromiseKit
 
 class TripDetailViewController: UIViewController {
     var labelText: String?
@@ -50,8 +52,40 @@ class TripDetailViewController: UIViewController {
 }
     
     @IBAction func acceptButtonTapped(_ sender: UIButton) {
+         let alert = UIAlertController(title: "Claim Trip", message: "Would you like to claim this trip ?", preferredStyle: .Alert)
+        let claimAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil)
+        myAlert.addAction(claimAction)
+        self.presentViewController(myAlert, animated: true, completion: nil);
     }
     
 
+//}
+
+///// if there is no error, completes with nil
+//public static func claimPickUp(trip: Trip, completion: @escaping (Swift.Error?) -> Void) {
+//    claim("pickUp", trip: trip, completion: completion)
+//}
+//
+///// if there is no error, completes with nil
+//public static func claimDropOff(trip: Trip, completion: @escaping (Swift.Error?) -> Void) {
+//    claim("dropOff", trip: trip, completion: completion)
+//}
+//
+//static func claim(_ key: String, trip: Trip, completion: @escaping (Swift.Error?) -> Void) {
+//    firstly {
+//        auth()
+//        }.then { _ -> Promise<User> in
+//            guard let uid = Auth.auth().currentUser?.uid else {
+//                throw Error.notAuthorized
+//            }
+//            return API.fetchUser(id: uid)
+//        }.then { user -> Void in
+//            Database.database().reference().child("trips").child(trip.key).updateChildValues([
+//                key: [user.key: user.name ?? "Anonymous Parent"]
+//                ])
+//            completion(nil)
+//        }.catch {
+//            completion($0)
+//    }
 //}
 
